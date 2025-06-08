9f1b333a83b02dbe34bf72f0a66d977e5e7ac916
Jan Bretscher <bretscherjan@gmail.com>
2025-06-08 13:02:54 +0200

    end day3

---
+++ 010_desktop/FlashCardsDesktop/FlashCardsDesktop/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using MySqlConnector;
+using System;
+using System.Windows;
+
+namespace FlashCardsDesktop
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        /*
+        private string host = "127.0.0.1";
+        private string database = "FlashCards";
+        private string username = "root";
+        private string password = "1234";
+        */
+        private string host = "herkules.net.letsbuild.ch";
+        private string database = "FlashCards";
+        private string username = "jan";
+        private string password = "VEezZ85d";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void login_button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = $"Server={host};Port=3306;Database={database};Uid={username};Pwd={password};Connection Timeout=30;";
+            Execute(connectionString, "SELECT * FROM users;");
+        }
+
+        public static void Execute(string connectionString, string sqlCommand, MySqlConnector.MySqlConnection connection = null)
+        {
+            try
+            {
+                if (connection == null)
+                {
+                    connection = new MySqlConnection(connectionString);
+                    Console.WriteLine("New connection created.");
+                }
+
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                    Console.WriteLine("Connection opened.");
+                }
+
+                MySqlCommand cmd = new MySqlCommand(sqlCommand, connection);
+
+                Console.WriteLine(sqlCommand + "\n" + cmd);
+
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"MySQL Exception: {ex.Message}");
+            }
+            finally
+            {
+                if (connection != null && connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+        }
+    }
+}
---
+++ 010_desktop/test_FlashCardsDesktop/FlashCardsDesktop/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using MySqlConnector;
+
+namespace FlashCardsDesktop
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /*
+        private string host = "herkules.letsbuild.ch";
+        private string database = "FlashCards";
+        private string username = "jan";
+        private string password = "VEezZ85d";
+        */
+
+        private string host = "localhost";
+        private string database = "FlashCards";
+        private string username = "root";
+        private string password = "1234";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void login_button_Click(object sender, RoutedEventArgs e)
+        {
+            mysql_connection();
+        }
+
+        private void mysql_connection()
+        {
+            string connectionString = $"Server={host};Port=3306;Database={database};User ID={username};Password={password};SslMode=None";
+
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+            }
+        }
+    }
+}
